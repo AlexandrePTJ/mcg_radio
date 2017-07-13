d48578980e1cca3c0eeee409230068aa85d90da5
Alexandre Petitjean <alpetitjean@gmail.com>
2017-07-13 20:41:53 +0200

    Play audio

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 import asyncio
-import functools
-import os
+import json
 import signal
 
-from mcg_radio.display_controller import DisplayController
-from mcg_radio.buttons_listener import ButtonsListener
+#from mcg_radio.display_controller import DisplayController
+#from mcg_radio.buttons_listener import ButtonsListener
 from mcg_radio.playback_controller import PlaybackController
 
 
 class McgRadio:
-    def __init__(self):
-        self.loop = asyncio.get_event_loop()
-        for sign in ('SIGINT', 'SIGTERM'):
-            self.loop.add_signal_handler(getattr(signal, sign),
-                                         functools.partial(self.ask_exit, sign))
 
-    def ask_exit(self, signame):
-        print("got signal %s: exit" % signame)
-        self.loop.stop()
+    def __init__(self):
+        pass
+
+    def load_radios(self, radios_json):
+        with open(radios_json) as f:
+            self.radios = json.load(f)
 
     def run(self):
-        dc = DisplayController()
-        dc.setup()
-        pc = PlaybackController(dc)
-        btl = ButtonsListener(pc)
+        PlaybackController.start_glib_loop()
+        #dc = DisplayController()
+        #dc.setup()
+        pc = PlaybackController(None)
+        pc.update_radios(self.radios)
+        pc.play("1")
+        #btl = ButtonsListener(pc)
+
+        loop = asyncio.get_event_loop()
+        loop.add_signal_handler(
+            signal.SIGINT,
+            loop.run_until_complete,
+            loop.shutdown_asyncgens())
 
         try:
-            print("Event loop running forever, press Ctrl+C to interrupt.")
-            print("pid %s: send SIGINT or SIGTERM to exit." % os.getpid())
-            self.loop.run_forever()
+            loop.run_forever()
         finally:
-            self.loop.run_until_complete(self.loop.shutdown_asyncgens())
-            self.loop.close()
+            loop.close()
+            pc.stop()
+            PlaybackController.stop_glib_loop()
 
 
 if __name__ == '__main__':
     mr = McgRadio()
+    mr.load_radios('radios.json')
     mr.run()
--- mcg_radio/playback_controller.py
+++ mcg_radio/playback_controller.py
@@ -1,8 +1,56 @@
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GObject
 
+import threading
 
 class PlaybackController:
+
+    glib_loop = None
+    glib_thread = None
+
     def __init__(self, display_controller):
-        pass
+        self.radios = dict()
+
+        self.player = Gst.ElementFactory.make('playbin', 'player')
+        bus = self.player.get_bus()
+        bus.enable_sync_message_emission()
+        bus.add_signal_watch()
+        bus.connect('message::tag', self._on_message)
+
+    @classmethod
+    def start_glib_loop(cls):
+        GObject.threads_init()
+        Gst.init(None)
+        cls.glib_loop = GObject.MainLoop()
+        cls.glib_thread = threading.Thread(target=cls.glib_loop.run)
+        cls.glib_thread.start()
+
+    @classmethod
+    def stop_glib_loop(cls):
+        cls.glib_loop.quit()
+        cls.glib_thread.join()
+        cls.glib_loop = cls.glib_thread = None
+
+    def _on_message(self, bus, message):
+        taglist = message.parse_tag()
+        for x in range(taglist.n_tags()):
+            name = taglist.nth_tag_name(x)
+            if name == 'title':
+                print('%s' % (taglist.get_string(name)[1]))
+
+    def update_radios(self, radios):
+        self.radios = radios
+
+    def play(self, index=None):
+        if index is None:
+            return
+        if index in self.radios:
+            self.player.set_property('uri', self.radios[index]['url'])
+            self.player.set_state(Gst.State.PLAYING)
+
+    def stop(self):
+        self.player.set_state(Gst.State.NULL)
 
     def next(self):
         print('next')
