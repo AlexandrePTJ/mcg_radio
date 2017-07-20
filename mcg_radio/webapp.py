3f562f878610cf3b5ec8d536f9d9b8d03bf5ceec
Alexandre Petitjean <alpetitjean@gmail.com>
2017-07-20 23:32:29 +0200

    Show radios

--- mcg_radio/webapp.py
+++ mcg_radio/webapp.py
@@ -4,19 +4,27 @@ import jinja2
 
 
 class RadiosView(web.View):
-	
-	@aiohttp_jinja2.template('radios.html')
-	async def get(self):
-		return { 'radios': self.request.app['PlaybackController'].radios }
 
-	async def post(self):
-		pass
+    MAX_RADIOS = 6
+
+    @aiohttp_jinja2.template('radios.html')
+    async def get(self):
+        radios = self.request.app['PlaybackController'].radios
+        if len(radios) < self.MAX_RADIOS:
+            for idx in range(len(radios) + 1, self.MAX_RADIOS):
+                radios.append({ 'label': '', 'url': '', 'index': idx })
+        return { 'radios': radios }
+
+    async def post(self):
+        data = await self.request.post()
+        print(data)
+        return web.HTTPFound('/')
 
 
 def make_webapp(playback_controller):
-	app = web.Application()
-	aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
-	app.router.add_static('/static', 'static')
-	app.router.add_route('*', '/', RadiosView)
-	app['PlaybackController'] = playback_controller
-	return app
+    app = web.Application()
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
+    app.router.add_static('/static', 'static')
+    app.router.add_route('*', '/', RadiosView)
+    app['PlaybackController'] = playback_controller
+    return app
