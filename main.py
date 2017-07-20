f2e1ea4ea073fd867ff9fa70122460e959fe931f
Alexandre Petitjean <alpetitjean@gmail.com>
2017-07-20 22:13:16 +0200

    Start web app

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from aiohttp import web
 from mcg_radio.fake_display_controller import FakeDisplayController as DisplayController
 #from mcg_radio.buttons_listener import ButtonsListener
 from mcg_radio.playback_controller import PlaybackController
-from mcg_radio.webapp import index
+from mcg_radio.webapp import make_webapp
 
 dc = DisplayController()
 dc.setup()
 
 pc = PlaybackController(dc)
 pc.update_radios('radios.json')
-pc.play(1)
+#pc.play(1)
 
 # btl = ButtonsListener(pc)
 
@@ -19,7 +19,4 @@ pc.play(1)
 gbulb.install()
 
 # WebApp is used as loop.run_forever() bridge
-app = web.Application()
-app.router.add_get('/', index)
-app['PlaybackController'] = pc
-web.run_app(app, host='127.0.0.1', port=8080)
+web.run_app(make_webapp(pc), host='127.0.0.1', port=8080)
--- mcg_radio/webapp.py
+++ mcg_radio/webapp.py
@@ -1,6 +1,22 @@
 from aiohttp import web
+import aiohttp_jinja2
+import jinja2
 
 
-async def index(request):
-    request.app['PlaybackController'].next()
-    return web.Response(text='Hello Aiohttp!')
+class RadiosView(web.View):
+	
+	@aiohttp_jinja2.template('radios.html')
+	async def get(self):
+		return { 'radios': self.request.app['PlaybackController'].radios }
+
+	async def post(self):
+		pass
+
+
+def make_webapp(playback_controller):
+	app = web.Application()
+	aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates'))
+	app.router.add_static('/static', 'static')
+	app.router.add_route('*', '/', RadiosView)
+	app['PlaybackController'] = playback_controller
+	return app
