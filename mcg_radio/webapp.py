6e09aa482382371a4e998186a2b0e7bb9b13972e
Alexandre Petitjean <alpetitjean@gmail.com>
2017-07-21 10:02:30 +0200

    Set POST

--- mcg_radio/webapp.py
+++ mcg_radio/webapp.py
@@ -5,19 +5,25 @@ import jinja2
 
 class RadiosView(web.View):
 
-    MAX_RADIOS = 6
+    MAX_RADIOS = 11
 
     @aiohttp_jinja2.template('radios.html')
     async def get(self):
         radios = self.request.app['PlaybackController'].radios
         if len(radios) < self.MAX_RADIOS:
             for idx in range(len(radios) + 1, self.MAX_RADIOS):
-                radios.append({ 'label': '', 'url': '', 'index': idx })
-        return { 'radios': radios }
+                radios.append({'label': '', 'url': '', 'index': idx})
+        return {'radios': radios}
 
     async def post(self):
         data = await self.request.post()
-        print(data)
+        radios = []
+        for n in range(1, self.MAX_RADIOS):
+            lbl_n = data['label_' + str(n)]
+            url_n = data['url_' + str(n)]
+            if lbl_n and url_n:
+                radios.append({'label': lbl_n, 'url': url_n, 'index': n})
+        self.request.app['PlaybackController'].radios = radios
         return web.HTTPFound('/')
 
 
