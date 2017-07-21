b3ff9bf1dce87192700b2f7cba36bc02b60f6970
Alexandre Petitjean <alpetitjean@gmail.com>
2017-07-21 09:27:27 +0200

    Add Fake class to ease dev on win

---
+++ dev_main_win.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+
+from mcg_radio.fake_mcg import FakeDisplayController as DisplayController
+from mcg_radio.fake_mcg import FakePlaybackController as PlaybackController
+from mcg_radio.webapp import make_webapp
+
+dc = DisplayController()
+dc.setup()
+
+pc = PlaybackController(dc)
+pc.update_radios('radios.json')
+
+# WebApp is used as loop.run_forever() bridge
+web.run_app(make_webapp(pc), host='127.0.0.1', port=8080)
--- mcg_radio/fake_display_controller.py
+++
@@ -1,9 +0,0 @@
-class FakeDisplayController:
-    def __init__(self):
-        pass
-
-    def setup(self):
-        pass
-
-    def set_message(self, message):
-        print(message)
